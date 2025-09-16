3acdd940a08f842da5f78008a20f73d9d721c682
AKSHAT GUPTA <akshat8036@gmail.com>
2025-09-16 11:43:39 +0530

    gradient desecnt and saving model using pickle

--- machine-learning-basics/lecture-4/gradient.py
+++ machine-learning-basics/lecture-4/gradient.py
@@ -1,14 +1,8 @@
 import numpy as np
 
 def gradient_d(x,y):
-    m_curr = b_curr = 0
-    iteratios = 1000
-    n = len(x)
-    for i in range(iteratios):
-        y_pred = m_curr*x +b_curr
+    pass
         
-
-
 x = np.array[1,2,3,4,5]
 y= np.array[4,6,8,10,12]
 
